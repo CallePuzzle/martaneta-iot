4441bbfefc754f13960542667193f37b1dd3fdfa
César M. Cristóbal <cmartin@bitban.com>
2023-08-13 23:41:41 +0200

    manifest to test nginx

--- k0s/main.tf
+++ k0s/main.tf
@@ -16,4 +16,13 @@ module "oci-k0s" {
 
   compartment_id  = "ocid1.tenancy.oc1..aaaaaaaa5ii3uidynoqhjub5ub66fm3ryn2my6txw6xrguihckyr2uyarlkq"
   k0s_config_path = "${path.root}/k0sctl.yaml"
+
+  manifests_source = {
+    repoURL = "https://github.com/CallePuzzle/martaneta-iot"
+    targetRevision = "HEAD"
+    path = "argo-manifests"
+    directory = {
+      recurse: true
+    }
+  }
 }
