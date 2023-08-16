edfaf0abf7e706019268a84fb429d7f290832edd
César M. Cristóbal <cmartin@bitban.com>
2023-08-16 17:40:54 +0200

    feat: enable argocd host

--- k0s/main.tf
+++ k0s/main.tf
@@ -17,6 +17,8 @@ module "oci-k0s" {
   compartment_id  = "ocid1.tenancy.oc1..aaaaaaaa5ii3uidynoqhjub5ub66fm3ryn2my6txw6xrguihckyr2uyarlkq"
   k0s_config_path = "${path.root}/k0sctl.yaml"
 
+  argocd_host = "argocd-martaneta.callepuzzle.com"
+
   manifests_source = {
     repoURL = "https://github.com/CallePuzzle/martaneta-iot"
     targetRevision = "HEAD"
